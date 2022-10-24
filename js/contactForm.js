53794cca437b78959fffbc7545b3a59ae7b20a46
StevanVS <stevanvs@outlook.com>
2022-10-24 00:46:53 -0500

    Mostrar mensaje al faltar campos en el form

--- js/contactForm.js
+++ js/contactForm.js
@@ -5,6 +5,10 @@ const telephoneInput = document.querySelector("[data-telephone-input]");
 const subjectInput = document.querySelector("[data-subject-input]");
 const messageInput = document.querySelector("[data-message-input]");
 
+form.addEventListener("input", (e) => {
+  clearErrorFor(e.target);
+});
+
 form.addEventListener("submit", (e) => {
   e.preventDefault();
 
@@ -16,19 +20,37 @@ form.addEventListener("submit", (e) => {
   const subject = subjectInput.value;
   const message = messageInput.value;
 
+  let error = false;
+  let textError = "Falta completar los siguientes campos:";
+
   if (names.trim().length === 0) {
-    return;
+    textError += "\n\t Nombres";
+    error = true;
+    setErrorFor(namesInput);
   }
   if (lastnames.trim().length === 0) {
-    return;
+    textError += "\n\t Apellidos";
+    error = true;
+    setErrorFor(lastnamesInput);
   }
   if (telephone.trim().length === 0) {
-    return;
+    textError += "\n\t Teléfono";
+    error = true;
+    setErrorFor(telephoneInput);
   }
   if (subject.trim().length === 0) {
-    return;
+    textError += "\n\t Asunto";
+    error = true;
+    setErrorFor(subjectInput);
   }
   if (message.trim().length === 0) {
+    textError += "\n\t Mensaje";
+    error = true;
+    setErrorFor(messageInput);
+  }
+
+  if (error) {
+    alert(textError);
     return;
   }
 
@@ -40,3 +62,11 @@ form.addEventListener("submit", (e) => {
   subjectInput.value = "";
   messageInput.value = "";
 });
+
+function setErrorFor(element) {
+  element.classList.add("error");
+}
+function clearErrorFor(element) {
+  element.classList.remove("error");
+}
+function clearAllErrors() {}
